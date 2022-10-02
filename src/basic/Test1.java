13edb0bf991bf180fe60abd7f4c77fdb3ebc0dd4
VIRAJ <VIRAJ@DESKTOP-U6OK72A>
2022-10-02 08:27:51 +0530

    First Commit

---
+++ src/basic/Test1.java
@@ -0,0 +1,19 @@
+package basic;
+
+public class Test1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("main method is running");
+		
+		m1();
+	}
+
+	public static void m1() 
+	{
+		System.out.println("Hi");
+	}
+	
+	
+	
+}
